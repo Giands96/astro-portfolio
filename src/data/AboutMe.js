c9ff66ed7bc51fc00d59ee534713881058edf1be
Axel <dsg4857@gmail.com>
2025-03-11 17:01:40 -0500

    About section completed

---
+++ src/data/AboutMe.js
@@ -0,0 +1,12 @@
+// src/data/AboutMeData.js
+import fdp from '../assets/fdp.jpg';
+
+export const AboutMeData = {
+  name: 'Axel Del Solar',
+  shortDescription: 'Soy un desarrollador frontend apasionado por crear experiencias digitales intuitivas, funcionales y visualmente impactantes.',
+  description: 'Soy un desarrollador frontend apasionado por la creación de interfaces modernas y funcionales. Me especializo en tecnologías como HTML, CSS, JavaScript y frameworks como React y Tailwind CSS. Mi objetivo es ofrecer experiencias de usuario excepcionales.',
+  skills: ["HTML/CSS", "JavaScript", "React", "Node.js", "Astro", "Tailwind CSS", "Git", "Responsive Design"],
+  resumeUrl: "/docs/mi-curriculum.pdf",
+  resumeButtonText: "Descargar CV"
+
+};
