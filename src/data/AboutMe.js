4f400a00e6940166f57318c752e62db509d12051
Axel <dsg4857@gmail.com>
2025-03-12 19:55:09 -0500

    Pasar a mejor una SPA, scrollbehavior agregado

--- src/data/AboutMe.js
+++ src/data/AboutMe.js
@@ -6,7 +6,7 @@ export const AboutMeData = {
   shortDescription: 'Soy un desarrollador frontend apasionado por crear experiencias digitales intuitivas, funcionales y visualmente impactantes.',
   description: 'Soy un desarrollador frontend apasionado por la creación de interfaces modernas y funcionales. Me especializo en tecnologías como HTML, CSS, JavaScript y frameworks como React y Tailwind CSS. Mi objetivo es ofrecer experiencias de usuario excepcionales.',
   skills: ["HTML/CSS", "JavaScript", "React", "Astro", "Tailwind CSS", "Git", "Responsive Design"],
-  resumeUrl: "/docs/mi-curriculum.pdf",
+  resumeUrl: "../docs/curriculum-vitae.pdf",
   resumeButtonText: "Descargar CV"
 
 };
