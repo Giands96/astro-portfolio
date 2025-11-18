18c13923fe2e8897e65d9910b96aa2ea7b557b70
Axel <dsg4857@gmail.com>
2025-11-18 15:21:27 -0500

    patch: Habilidades corregidas

--- src/data/AboutMe.js
+++ src/data/AboutMe.js
@@ -5,7 +5,7 @@ export const AboutMeData = {
   name: 'Axel Del Solar',
   shortDescription: 'Soy un desarrollador frontend apasionado por crear experiencias digitales intuitivas, funcionales y visualmente impactantes.',
   description: 'Soy un desarrollador frontend apasionado por la creación de interfaces modernas y funcionales. Me especializo en tecnologías como HTML, CSS, JavaScript y frameworks como React y Tailwind CSS. Mi objetivo es ofrecer experiencias de usuario excepcionales.',
-  skills: ["HTML/CSS", "JavaScript", "React", "Astro", "Tailwind CSS", "Git", "Responsive Design"],
+  skills: ["HTML/CSS", "JavaScript", "React", "Astro", "Tailwind CSS", "Git", "Responsive Design","Spring Boots", "MySQL", "UX Design"],
   resumeUrl: "../docs/curriculum-vitae.pdf",
   resumeButtonText: "Descargar CV",
   email: "axeldelsolar.dev@gmail.com"
